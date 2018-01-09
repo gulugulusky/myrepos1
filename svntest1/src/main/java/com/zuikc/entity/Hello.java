dcac20b79f681e69cdd66a37d39e229b67b6d10d
gulugulusky <15656230967@163.com>
2018-01-09 15:40:32 +0800

    sadfsadf

---
+++ svntest1/src/main/java/com/zuikc/entity/Hello.java
@@ -0,0 +1,11 @@
+package com.zuikc.entity;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("svn.......");
+		System.out.println("nihao");
+	}
+
+}
